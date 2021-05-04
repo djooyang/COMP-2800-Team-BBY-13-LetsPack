71bebf8cb4f19a55a05683179dc7c8b67afaeca5
Daniel Yang <danjh.yang@gmail.com>
2021-05-04 11:56:48 -0700

    added json file

---
+++ data/user.js
@@ -0,0 +1,13 @@
+let user = [
+    {name: 'Rob', address: '123 Fake street', dob: "April 27, 2021", sex: 'male', hobby: 'canoeing'},
+    {name: 'Dan', address: '456 Fake street', dob: "January 02, 2021", sex: 'male', hobby: 'hiking'},
+    {name: 'Damon', address: '444 Tutor drive', dob: "April 27, 2021", sex: 'male', hobby: 'basketball'},
+    {name: 'Connor', address: '1999 Fake street', dob: "April 27, 2021", sex: 'male', hobby: 'gaming'},
+    {name: 'Francois', address: '1555 aisle street', dob: "April 27, 2021", sex: 'male', hobby: 'dancing'},
+    {name: 'Chris', address: '4111 tremble street', dob: "April 27, 2021", sex: 'male', hobby: 'archery'},
+    {name: 'Philip', address: '1231 Fake street', dob: "April 27, 2021", sex: 'male', hobby: 'hunting'},
+    {name: 'Tristan', address: '333 arco street', dob: "April 27, 2021", sex: 'male', hobby: 'lancing'},
+    {name: 'Arron', address: '3113 Ferguson street', dob: "April 27, 2021", sex: 'male', hobby: 'fencing'},
+    {name: 'Terry', address: '4141 Lee street', dob: "April 27, 2021", sex: 'male', hobby: 'tap dancing'},
+];
+
