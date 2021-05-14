61f822cbaeab41492c05684deddc1facf6308f88
Robert Roe <robert.james.roe@gmail.com>
2021-05-13 22:50:16 -0700

    Fixed bug where item name wouldnt update.

--- server/services/render.js
+++ server/services/render.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 //gets all the users and passes them to the index page when it's constructed
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('https://letspack.herokuapp.com/api/users')
+    axios.get('http://localhost:3000/api/users')
         .then(function(response){
             res.render('login', { users : response.data });
         })
@@ -68,7 +68,7 @@ exports.signup = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('https://letspack.herokuapp.com/api/users', { params : { id : req.query.id }})
+    axios.get('http://localhost:3000//api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
@@ -78,7 +78,7 @@ exports.update_user = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('https://letspack.herokuapp.com/api/users', { params : { id : req.query.id }})
+    axios.get('http://localhost:3000//api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
